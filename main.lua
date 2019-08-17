2532451ffb1700f1df79de2ff53858c0dd6dec83
Lauri Tirkkonen <lotheac@iki.fi>
2019-08-17 16:18:37 +0300

    implement in-combat show/hide & avoid taint
    this is essentially a reimplementation that just uses show/hide on the
    relevant frames, and avoids touching multiactionbar configuration.
    modifying the config through the interface options frames is not doable
    in the restricted environment since Click()ing the checkboxes is not
    available, and we can't touch the globals that make up those settings
    either.

--- main.lua
+++ main.lua
@@ -2,53 +2,54 @@
 local eh = CreateFrame("Frame", "TBars", UIParent)
 eh:RegisterEvent("ADDON_LOADED")
 
-local shown
+function eh:UpdateUnprotectedVisibility()
+  local frames = { MicroButtonAndBagsBar }
+  for _, f in ipairs(frames) do
+    if f:IsProtected() then
+      print("ToggleBars error: " .. f:GetName() .. " is protected")
+    else
+      f:SetShown(MainMenuBarArtFrame:IsShown())
+    end
+  end
+end
 
-function eh:HideBars()
-  SHOW_MULTI_ACTIONBAR_1 = nil
-  SHOW_MULTI_ACTIONBAR_2 = nil
-  SHOW_MULTI_ACTIONBAR_3 = nil
-  SHOW_MULTI_ACTIONBAR_4 = nil
-  InterfaceOptions_UpdateMultiActionBars()
-  -- MainMenuBarArtframe includes action buttons etc., but not the exp bar
+function eh:SetDefaults()
   MainMenuBarArtFrame:Hide()
   MicroButtonAndBagsBar:Hide()
-  shown = false
-end
-function eh:ShowBars()
-  SHOW_MULTI_ACTIONBAR_1 = "1"
-  SHOW_MULTI_ACTIONBAR_2 = "1"
-  SHOW_MULTI_ACTIONBAR_3 = "1"
-  SHOW_MULTI_ACTIONBAR_4 = "1"
-  InterfaceOptions_UpdateMultiActionBars()
-  MainMenuBarArtFrame:Show()
-  MicroButtonAndBagsBar:Show()
-  shown = true
-end
-function eh:ToggleBars()
-  if InCombatLockdown() then
-    print("Cannot toggle bars in combat")
-    return
-  end
-  if shown then
-    eh:HideBars()
-  else
-    eh:ShowBars()
-  end
+  -- MultiBarLeft:Hide() called from here may not actually hide it; most likely
+  -- something else shows it afterward. and because Blizzard, MultiBarLeft is
+  -- actually not contained in VerticalMultiBarsContainer. So just hook
+  -- MultiActionBar_Update to set visibility on both.
+  hooksecurefunc("MultiActionBar_Update", function()
+    local show = MainMenuBarArtFrame:IsShown()
+    MultiBarLeft:SetShown(show)
+    VerticalMultiBarsContainer:SetShown(show)
+  end)
 end
 
 function eh:InitAddon(ev, addon)
-  if not (ev == "ADDON_LOADED" and addon == "ToggleBars") then
-    return
-  end
-  eh:HideBars()
-  eh.togglebutton = CreateFrame("Button", "ToggleBarsButton", StatusTrackingBarManager)
+  if not (ev == "ADDON_LOADED" and addon == "ToggleBars") then return end
+  eh:SetDefaults()
+  -- MainMenuBarArtFrame is protected, so we have to use
+  -- SecureHandlerClickTemplate to show/hide it
+  eh.togglebutton = CreateFrame("BUTTON", "ToggleBarsButton", StatusTrackingBarManager, "SecureHandlerClickTemplate")
   eh.togglebutton.texture = eh.togglebutton:CreateTexture()
   eh.togglebutton.texture:SetAllPoints()
   eh.togglebutton.texture:SetColorTexture(0, 0, 0, 0.4)
   eh.togglebutton:SetWidth(16)
-  eh.togglebutton:SetPoint("TOPLEFT", StatusTrackingBarManager.SingleBarSmall, "TOPLEFT")
-  eh.togglebutton:SetPoint("BOTTOMLEFT", StatusTrackingBarManager.SingleBarSmall, "BOTTOMLEFT")
-  eh.togglebutton:SetScript("OnClick", eh.ToggleBars)
+  eh.togglebutton:SetHeight(16)
+  eh.togglebutton:SetPoint("TOPLEFT", MainMenuBar, "BOTTOMLEFT", 0, 0)
+  eh.togglebutton:SetFrameRef("main", MainMenuBarArtFrame)
+  eh.togglebutton:SetFrameRef("right", VerticalMultiBarsContainer)
+  eh.togglebutton:SetFrameRef("left", MultiBarLeft)
+  eh.togglebutton.unprivfunc = eh.UpdateUnprotectedVisibility
+  eh.togglebutton:SetAttribute("_onclick", [[
+  local toggleframes = newtable("main", "left", "right")
+  for _, ref in ipairs(toggleframes) do
+    local f = self:GetFrameRef(ref)
+    if f:IsShown() then f:Hide() else f:Show() end
+  end
+  owner:CallMethod("unprivfunc")
+  ]])
 end
 eh:SetScript("OnEvent", eh.InitAddon)
